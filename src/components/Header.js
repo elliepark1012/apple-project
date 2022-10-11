e53af4eab3429c725b4eec6dbb11c6b2acdaa5bf
Ellie Park <elliepark1012@gmail.com>
2022-10-11 15:08:42 -0400

    main lightbox isopen toggle

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,9 +5,6 @@ function Header ({ setSearchText }) {
 
     return (
         <header>
-        {/* <h1>
-            <img className="logo" src="/images/logo_transparent.png"/>
-        </h1> */}
       <SearchBar setSearchText={setSearchText} />
         </header>
     )
--- src/components/ListingCard.js
+++ src/components/ListingCard.js
@@ -1,48 +1,42 @@
-import React from "react";
+import React, {useState} from 'react';
 
-function ListingCard( {listing} ) {
+function ListingCard( {listing, children, Wrapper = 'div'} ) {
 
-  const myModal= (
-    <div id="myModal" className="modal">
-      <span className="close cursor"
-            onClick={closeModal}>
-        &times? 
-      </span>
-      <div className="modal-content">
-        <h1>{listing.name}</h1>
-        <img className="card-image" src={listing.image} />
-        <h3>{listing.price}</h3>
-        <h4>{listing.reviews.map((review) =>  (
-                <div>
-                  <h5>{review.ratings}</h5>
-                  <h6>{review.comment}</h6>
-                </div>
+    const [ isOpen, setIsOpen ] = useState(false);
+
+    const toggleIsOpen = () => {
+        setIsOpen(!isOpen)
+    };
+    
+    const main = (
+        <div key={listing.id} className="cards-containers">
+            <div className="cards-contents">
+                <h1>{listing.name}</h1>
+                <img className="card-image" src={listing.image} />
+            </div>
+        </div>
         )
-        )}
-        </h4>
-      </div>
-    </div>
-  )
 
-  console.log(myModal)
+    const lightbox = (
+                <div key={listing.id} className="modal-content">
+                    <h1>{listing.name}</h1>
+                    <img className="card-image" src={listing.image} />
+                    <h3>{listing.price}</h3>
+                    {listing.reviews.map((review) =>  (
+                        <div key={review.comment}>
+                            <h5>{review.ratings}</h5>
+                            <h6>{review.comment}</h6>
+                         </div>))}
+                    </div>
+                    )  
 
-  function openModal() {
-    return <div style={{display:'block'}}>{myModal}</div>
-  }
+    return (
+        <Wrapper>
+            {children}
+            {lightbox}
+        </Wrapper>
+    );
+};
 
-  function closeModal() {
-    return <div style={{display:'none'}}>{myModal}</div>
-  }
+export default ListingCard;
 
-
-  return (
-    <div className="cards-containers">
-      <div className="cards-contents">
-        <h1>{listing.name}</h1>
-        <img onClick={openModal} className="card-image" src={listing.image} />
-      </div>
-    </div>
-  )
-}
-
-export default ListingCard
